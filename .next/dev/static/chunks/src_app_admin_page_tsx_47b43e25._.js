7cc53850bd90da4788fdc4be4b3f04fad29b6268
MANIKUMAR <shawred.analytics@gmail.com>
2026-01-27 13:20:14 +0530

    Cleanup: Remove .next artifacts and unused watchdog dependency to fix deployment

--- .next/dev/static/chunks/src_app_admin_page_tsx_47b43e25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_2f6f16be._.js"
-],
-    source: "dynamic"
-});
